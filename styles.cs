14b05d0c02e13c37f805c48c72fbb881e9af6f26
kristalhellokitty <kristal.abreu@escola.pr.gov.br>
2025-10-20 13:08:16 -0300

    Update styles.cs

--- styles.cs
+++ styles.cs
@@ -1,3 +1,17 @@
-<hmtl lang="pt.BR"
+ody {
+    background-color: #1D4221;
+    color: white;
+    font-family: "Bai Jamjuree", sans-serif;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 700px;
+    margin: 0;
+}
 
-<body>
+button {
+    background-color: #64A712;
+    color: white;
+    font-family: "Bai Jamjuree", sans-serif;
+}
+
